2d4eb695f23c3204f0ddd828e02b1fb4c4bb6148
VinayakGaikwad <vinaayakgaikwad@gmail.com>
2025-03-09 22:01:01 +0530

    Cors fix

--- server/index.js
+++ server/index.js
@@ -35,21 +35,28 @@ app.use(function(req, res, next) {
     'server-researchrx.vercel.app'
   ];
   
-  const origin = req.get('origin') || req.get('referrer');
+  const origin = req.get('origin');
   
-  whitelist.forEach(function(val) {
-    if (origin && origin.indexOf(val) > -1) {
+  if (origin) {
+    const isWhitelisted = whitelist.some(domain => origin.includes(domain));
+    if (isWhitelisted) {
       res.setHeader('Access-Control-Allow-Origin', origin);
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
       res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Cookie');
       res.setHeader('Access-Control-Allow-Credentials', 'true');
       res.setHeader('Access-Control-Expose-Headers', 'set-cookie');
     }
-  });
+  }
 
   // Handle preflight requests
   if (req.method === 'OPTIONS') {
-    return res.status(204).end();
+    res.setHeader('Access-Control-Allow-Origin', origin || '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Cookie');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Expose-Headers', 'set-cookie');
+    res.status(200).end();
+    return;
   }
 
   next();
